2fcc6a21345c19fe9615827d8f07c346f7d6ef29
Malthe Jørgensen <malthe.banan@gmail.com>
2012-11-12 03:21:57 +0100

    Add better interrupt handling (Ctrl-C)
    Now in `ghci Surf.hs`, a Ctrl-C will close the socket, so that ghci
    doesn't have to be restarted, when interrupting the server.

--- Surf.hs
+++ Surf.hs
@@ -1,18 +1,43 @@
-import Network
+import System.IO as IO
+import Network as N
 {-import GHC.IO.Handle-}
-import Data.ByteString -- for hGetContents
-import qualified Data.ByteString.Char8 as C -- for pack
+import Data.ByteString as BS hiding (putStrLn, hPutStrLn) -- for hGetContents
+import qualified Data.ByteString.Char8 as C-- for pack
 import Data.Attoparsec.ByteString
 import Control.Applicative -- for <|>
 {-import qualified Data.ByteString as B-}
+import Control.Exception as E
+import Control.Concurrent
+import System.Posix.Signals
 
 port = 8000
 
+
+-- http://en.wikipedia.org/wiki/HTTP_persistent_connection
+-- "Connection: Keep-Alive" is the default so we cannot use `hGetContents` as 
+-- it will block.
+
+
 main = do
+  tid <- myThreadId
+  -- installHandler keyboardSignal (Catch (throwTo tid UserInterrupt)) Nothing
+  -- installHandler sigINT (Catch (throwTo tid UserInterrupt)) Nothing
+  -- installHandler sigTERM (Catch (throwTo tid UserInterrupt)) Nothing
+  -- installHandler sigINT Ignore Nothing -- ignore Ctrl-C
+
   print $ "Started listening on port " ++ show port
-  sock <- listenOn (PortNumber port)
+  sock <- N.listenOn (PortNumber port)
+  acceptConn sock `E.catch` interrupt sock
+  where interrupt sock exception =
+          let err = show (exception :: SomeException) in do
+            putStrLn "Closing socket."
+            hPutStrLn stderr $ "Some exception caught: " ++ err
+            sClose sock
+
+acceptConn sock = do
   (handle, hostname, portnumber) <- accept sock
-  text <- hGetContents handle
+  -- print hostname ++ " " ++ portnumber
+  text <- BS.hGetLine handle
   print text
   sClose sock
 
